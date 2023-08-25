5322d75266380cb7a65910c479fd890c09df5d89
Stefania <stebofof@gmail.com>
2023-08-25 13:02:50 +0200

    Get coordinates to display forecast

--- src/index.js
+++ src/index.js
@@ -113,6 +113,15 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+// Forecast prediction
+
+function getForecast(coordinates) {
+  let apiKey = "0efb4fc16a9ed98dc0b3aafd8491d6ad";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiURL);
+  axios.get(apiURL).then(displayForecast);
+}
+
 // Weather
 function displayWeather(response) {
   let currentHour = response.data.dt * 1000;
@@ -139,6 +148,8 @@ function displayWeather(response) {
   sunsetElement.innerHTML = formatSunsetTimestamp(response);
   currentWeatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`);
   currentWeatherIcon.setAttribute("alt", capitalizeFirstLetter(response.data.weather[0].description));
+
+  getForecast(response.data.coord);
 }
 
 function capitalizeFirstLetter(str) {
@@ -147,9 +158,9 @@ function capitalizeFirstLetter(str) {
 
 // Search Engine:
 function searchCity(city) {
-  var apiKey = "f4d1475cfe015c4c50b2300aa82dd590";
-  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeather);
+  let apiKey = "f4d1475cfe015c4c50b2300aa82dd590";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayWeather);
 }
 
 function showCitySearched(event) {
@@ -193,5 +204,3 @@ fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 // Set up a default city
 searchCity("Florence");
-
-displayForecast();
